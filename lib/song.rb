82c590f53fe8986e1e24a6a08ee013f0f2d094e2
Justin Branch <justinms@outlook.com>
2020-01-17 04:36:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,10 @@ class Song
   
   def self.find_by_name(song_name)
     self.all.find{|song| song.name == song_name}
-  end 
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    if self.all.include?()
     
   
 end
