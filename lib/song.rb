495147e0b199a757e0441b3963a7945d71083e46
Justin Branch <justinms@outlook.com>
2020-01-17 04:03:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,19 @@
 class Song
   attr_accessor :name, :artist_name
+  
+  # Class variable
   @@all = []
 
+  # Class method 
   def self.all
     @@all
   end
   
+  # Instance method 
   def save
     self.class.all << self
   end
   
-  def self.create
-    self.new.save
-  end 
+  
 
 end
